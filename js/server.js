5188fb7b5ba9caa2468ee0ced382dad1270685a9
huangruihao <huangruihaocst@126.com>
2016-03-09 21:46:20 +0800

    add a second source

--- js/server.js
+++ js/server.js
@@ -4,6 +4,7 @@
 
 /* 这些函数是应该写在服务端的 */
 var keyword = '清华';
+var api_key = '606ef48abce1cb59a5694142d87a64df';
 
 function queryFrom(source, queryString, callback) {
     // 这里写从真正的数据源获取数据, 并且统一成如下的数据格式
@@ -11,19 +12,18 @@ function queryFrom(source, queryString, callback) {
     // "image":"xxx"}
     switch (source) {
         /* TODO 从某个API获取数据, 转换成以上格式并且调用callback(data) */
-        case 'songshuxiansheng':
+        case '松鼠先生':
             $.ajax({
                 url: "http://apis.baidu.com/songshuxiansheng/real_time/search_news?keyword=" + keyword,
-                //data: { signature: authHeader },
                 type: "GET",
-                beforeSend: function(xhr){xhr.setRequestHeader('apikey', '606ef48abce1cb59a5694142d87a64df');},
+                beforeSend: function(xhr){xhr.setRequestHeader('apikey', api_key);},
                 success: function(data) {
-                    var news_hash = JSON.parse(data);
-                    var raw = news_hash['retData']['data'];//array
+                    var news = JSON.parse(data);
+                    var raw = news['retData']['data'];//array
                     var content = [];
                     for(var i = 0;i < raw.length; ++i){
                         var map = new Map();
-                        map.set('source', 'songshuxiansheng');
+                        map.set('source', '松鼠先生');
                         map.set('title', raw[i]['title']);
                         map.set('date', raw[i]['datetime']);
                         map.set('description', raw[i]['abstract']);
@@ -35,7 +35,28 @@ function queryFrom(source, queryString, callback) {
                 }
             });
             break;
-        case 'xxapi':
+        case 'show':
+            $.ajax({
+                url: "http://apis.baidu.com/showapi_open_bus/channel_news/search_news?title" + keyword,
+                type: "GET",
+                beforeSend: function(xhr){xhr.setRequestHeader('apikey', api_key);},
+                success: function(data) {
+                    var news = JSON.parse(data);
+                    var raw = news['showapi_res_body']['pagebean']['contentlist'];//array
+                    var content = [];
+                    for(var i = 0;i < raw.length; ++i){
+                        var map = new Map();
+                        map.set('source', 'show');
+                        map.set('title', raw[i]['title']);
+                        map.set('date', raw[i]['pubdate']);
+                        map.set('description', raw[i]['desc']);
+                        map.set('url', raw[i]['link']);
+                        map.set('image', raw[i]['imageurls']['url']);
+                        content.push(map);
+                    }
+                    callback(content);
+                }
+            });
             break;
         default: // 从所有数据源获取数据
             // 这里json/news.json是模拟一个数据源
--- js/view.js
+++ js/view.js
@@ -9,17 +9,21 @@ $(document).ready(function(){
     var queryString = [keyword, media, date].join(" ");
     // post /query?str=keyword, 得到一个json即result
     // 正常应该是post, 服务端会调用queryFromXXX, 然后返回一个json, 这里直接调用queryFromXXX了
-    queryFrom('songshuxiansheng', queryString, function(result) {
-        var content_list = $("#content_list");
-        for (var i = 0; i < result.length; ++i) {
-            var item = result[i];
-            // TODO 这里可以修改从而美化界面, 可用的字段在server.js可以找到
-            var url = item.get('url');
-            var source = item.get('source');
-            var title = item.get('title');
-            content_list.append("<li><a href=\"" +
-                url + "\">(From: " + source + ") " +
-                title + "</a></li>");
-        }
-    });
+    var sources = ['松鼠先生', 'show'];
+    for(var i = 0;i < sources.length; ++i){
+        queryFrom(sources[i], queryString, function(result) {
+            var content_list = $("#content_list");
+            for (var i = 0; i < result.length; ++i) {
+                var item = result[i];
+                // TODO 这里可以修改从而美化界面, 可用的字段在server.js可以找到
+                var url = item.get('url');
+                var source = item.get('source');
+                var title = item.get('title');
+                content_list.append("<li><a href=\"" +
+                    url + "\">(From: " + source + ") " +
+                    title + "</a></li>");
+            }
+        });
+    }
+
 });
