c58c71c309f08e0a9726229293fcf2e218d7c323
huangruihao <huangruihaocst@126.com>
2016-03-09 21:28:13 +0800

    add the first source

--- js/server.js
+++ js/server.js
@@ -3,12 +3,37 @@
  */
 
 /* 这些函数是应该写在服务端的 */
+var keyword = '清华';
+
 function queryFrom(source, queryString, callback) {
     // 这里写从真正的数据源获取数据, 并且统一成如下的数据格式
-    // { "source": "sogou", "title": "xxx", "date": "20150101", "description": "xxx", "url": "http://www.baidu.com" }
+    // { "source": "sogou", "title": "xxx", "date": "20150101", "description": "xxx", "url": "http://www.baidu.com",
+    // "image":"xxx"}
     switch (source) {
-        case 'baidu':
-            /* TODO 从某个API获取数据, 转换成以上格式并且调用callback(data) */
+        /* TODO 从某个API获取数据, 转换成以上格式并且调用callback(data) */
+        case 'songshuxiansheng':
+            $.ajax({
+                url: "http://apis.baidu.com/songshuxiansheng/real_time/search_news?keyword=" + keyword,
+                //data: { signature: authHeader },
+                type: "GET",
+                beforeSend: function(xhr){xhr.setRequestHeader('apikey', '606ef48abce1cb59a5694142d87a64df');},
+                success: function(data) {
+                    var news_hash = JSON.parse(data);
+                    var raw = news_hash['retData']['data'];//array
+                    var content = [];
+                    for(var i = 0;i < raw.length; ++i){
+                        var map = new Map();
+                        map.set('source', 'songshuxiansheng');
+                        map.set('title', raw[i]['title']);
+                        map.set('date', raw[i]['datetime']);
+                        map.set('description', raw[i]['abstract']);
+                        map.set('url', raw[i]['url']);
+                        map.set('image', raw[i]['image_url']);
+                        content.push(map);
+                    }
+                    callback(content);
+                }
+            });
             break;
         case 'xxapi':
             break;
--- js/view.js
+++ js/view.js
@@ -9,12 +9,17 @@ $(document).ready(function(){
     var queryString = [keyword, media, date].join(" ");
     // post /query?str=keyword, 得到一个json即result
     // 正常应该是post, 服务端会调用queryFromXXX, 然后返回一个json, 这里直接调用queryFromXXX了
-    queryFrom("all", queryString, function(result) {
+    queryFrom('songshuxiansheng', queryString, function(result) {
         var content_list = $("#content_list");
         for (var i = 0; i < result.length; ++i) {
             var item = result[i];
             // TODO 这里可以修改从而美化界面, 可用的字段在server.js可以找到
-            content_list.append("<li><a href=\"" + item.url + "\">(From: " + item.source + ") " + item.title + "</a></li>");
+            var url = item.get('url');
+            var source = item.get('source');
+            var title = item.get('title');
+            content_list.append("<li><a href=\"" +
+                url + "\">(From: " + source + ") " +
+                title + "</a></li>");
         }
     });
 });
