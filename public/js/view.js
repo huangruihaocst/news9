1a467a305388bf69f2d3effe300abb359324209f
huangruihao <huangruihaocst@126.com>
2016-03-14 19:43:16 +0800

    fixed the problem of img

--- public/js/view.js
+++ public/js/view.js
@@ -1,9 +1,10 @@
 /**
  * Created by huangruihao on 16-3-7.
  */
-var JQuery = require('jquery');
+var $ = require('jquery');
 var API_HOST = 'news.net9.org';
 var HTTP_SCHEME = 'https://';
+var MAX_SIZE = 128;
 
 var siteMap = [
     [/qq\.com/, "腾讯"],
@@ -43,12 +44,20 @@ function sourceAnalyzer(url) {
     return 'Internet';
 }
 
-JQuery(document).ready(function(){
+function isValid(item){
+    var url = item.url;
+    var title = item.title;
+    var date = item.date;
+    var source = item.source;
+    return url != null && title != null && date != null && source != null;
+}
+
+$(document).ready(function(){
     var keyword = "thu";
     var media = "";
     var date = "";
     var queryString = [keyword, media, date].join(" ");
-    JQuery.ajax({
+    $.ajax({
         type: "GET",
         url: HTTP_SCHEME + API_HOST + "/api/news",
         crossDomain : true,
@@ -56,32 +65,61 @@ JQuery(document).ready(function(){
             withCredentials: true
         },
         success: function (result) {
-            var list = JQuery("#list");
+            var list = $("#list");
             for (var i = 0; i < result.length; ++i) {
                 var item = result[i];
                 // TODO 这里可以修改从而美化界面, 可用的字段在server.js可以找到
-                var url = item.url;
-                var title = item.title;
-                var image = item.image;
-                var date = item.date;
-                var content = item.description;
-                console.log(image);
-                var media = sourceAnalyzer(url);
-                var source = item.source;
-                if(source == '松鼠先生' || source == 'show'){
-                    source = media;
-                }
-                var description = source + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + date;
+                if(isValid(item)){
+                    var url = item.url;
+                    var title = item.title;
+                    var image = item.image;
+                    var date = item.date;
+                    var content = item.description;
+                    var media = sourceAnalyzer(url);
+                    var source = item.source;
+                    if(source == '松鼠先生' || source == 'show'){
+                        source = media;
+                    }
+                    var description = source + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + date;
 
-                if (date) {
                     var html = "<li>" +
                         "<div class='row'>" + "<div class='col-md-9'><a href=\"" + url + "\"><p>" +
                         title + "</p></a><p class='text-info'>" + description + "</p><p class='text-muted'>" +
                         content + "</p></div><div class='col-md-3'>" +
-                        "<img onerror='src=\"alt.jpg\";onerror=null;' src='" + image + "'/></div></li>";
+                        "<img class='scaled' onerror='src=\"alt.jpg\";onerror=null;' src='" + image +
+                        "'/></div></li>";
                     list.append(html);
                 }
+
             }
+            var scaled = document.getElementsByClassName('scaled');
+            console.log(scaled.length);
+            for(var j = 0;j < scaled.length; ++j){
+                scaled[j].onload = function(){
+                    var width = this.width;
+                    var height = this.height;
+                    if(width > height){
+                        this.width = MAX_SIZE;
+                        this.height = MAX_SIZE * height / width;
+                    }else{
+                        this.height = MAX_SIZE;
+                        this.width = MAX_SIZE * width / height;
+                    }
+                }
+            }
+            //for(var j = 0;j < scaled.length; ++j){
+            //    var original_width = scaled[j].clientWidth;
+            //    var original_height = scaled[j].clientHeight;
+            //    console.log(original_width);
+            //}
+
+            //if(original_height > original_width){
+            //    scaled.height(MAX_SIZE);
+            //    scaled.width();
+            //}else{
+            //    scaled.width(MAX_SIZE);
+            //    scaled.height(MAX_SIZE * original_height / original_width);
+            //}
         }
     });
 });
