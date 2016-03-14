15ba1fbfb09824f2d3caea8de66a63dfbe5874c2
Alex Wang <ice_b0und@hotmail.com>
2016-03-14 20:45:23 +0800

    fix the fucking merge problems

--- public/js/view.js
+++ public/js/view.js
@@ -52,71 +52,67 @@ function isValid(item){
     var source = item.source;
     return url != null && title != null && date != null && source != null;
 }
+function initPage() {
+    var keyword = "thu";
+    var media = "";
+    var date = "";
+    var queryString = [keyword, media, date].join(" ");
+    $.ajax({
+        type: "GET",
+        url: HTTP_SCHEME + API_HOST + "/api/news",
+        crossDomain: true,
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function (result) {
+            var list = $("#list");
+            for (var i = 0; i < result.length; ++i) {
+                var item = result[i];
+                // TODO 这里可以修改从而美化界面, 可用的字段在server.js可以找到
+                if (isValid(item)) {
+                    var url = item.url;
+                    var title = item.title;
+                    var image = item.image;
+                    var date = item.date;
+                    var content = item.description;
+                    var media = sourceAnalyzer(url);
+                    var source = item.source;
+                    if (source == '松鼠先生' || source == 'show') {
+                        source = media;
+                    }
+                    var description = source + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + dateFormat(date, "yyyy-mm-dd, hh:MM:ss");
+                    if (source == '松鼠先生' || source == 'show') {
+                        source = media;
+                    }
 
-$(document).ready(function() {
-    function initPage() {
-        var keyword = "thu";
-        var media = "";
-        var date = "";
-        var queryString = [keyword, media, date].join(" ");
-        $.ajax({
-            type: "GET",
-            url: HTTP_SCHEME + API_HOST + "/api/news",
-            crossDomain: true,
-            xhrFields: {
-                withCredentials: true
-            },
-            success: function (result) {
-                var list = $("#list");
-                for (var i = 0; i < result.length; ++i) {
-                    var item = result[i];
-                    // TODO 这里可以修改从而美化界面, 可用的字段在server.js可以找到
-                    if (isValid(item)) {
-                        var url = item.url;
-                        var title = item.title;
-                        var image = item.image;
-                        var date = item.date;
-                        var content = item.description;
-                        var media = sourceAnalyzer(url);
-                        var source = item.source;
-                        if (source == '松鼠先生' || source == 'show') {
-                            source = media;
-                        }
-                        var description = source + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + dateFormat(date, "yyyy-mm-dd, hh:MM:ss");
-                        if (source == '松鼠先生' || source == 'show') {
-                            source = media;
-                        }
+                    var html = "<li>" +
+                        "<div class='row'>" + "<div class='col-md-9'><a href=\"" + url + "\" target='_blank'><p>" +
+                        title + "</p></a><p class='text-info'>" + description + "</p><p class='text-muted'>" +
+                        content + "</p></div><div class='col-md-3'>" +
+                        "<img class='scaled' onerror='src=\"alt.jpg\";onerror=null;' src='" + image +
+                        "'/></div></li>";
+                    list.append(html);
 
-                        var html = "<li>" +
-                            "<div class='row'>" + "<div class='col-md-9'><a href=\"" + url + "\" target='_blank'><p>" +
-                            title + "</p></a><p class='text-info'>" + description + "</p><p class='text-muted'>" +
-                            content + "</p></div><div class='col-md-3'>" +
-                            "<img class='scaled' onerror='src=\"alt.jpg\";onerror=null;' src='" + image +
-                            "'/></div></li>";
-                        list.append(html);
-
-                        var scaled = document.getElementsByClassName('scaled');
-                        console.log(scaled.length);
-                        for (var j = 0; j < scaled.length; ++j) {
-                            scaled[j].onload = function () {
-                                var width = this.width;
-                                var height = this.height;
-                                if (width > height) {
-                                    this.width = MAX_SIZE;
-                                    this.height = MAX_SIZE * height / width;
-                                } else {
-                                    this.height = MAX_SIZE;
-                                    this.width = MAX_SIZE * width / height;
-                                }
+                    var scaled = document.getElementsByClassName('scaled');
+                    console.log(scaled.length);
+                    for (var j = 0; j < scaled.length; ++j) {
+                        scaled[j].onload = function () {
+                            var width = this.width;
+                            var height = this.height;
+                            if (width > height) {
+                                this.width = MAX_SIZE;
+                                this.height = MAX_SIZE * height / width;
+                            } else {
+                                this.height = MAX_SIZE;
+                                this.width = MAX_SIZE * width / height;
                             }
                         }
                     }
                 }
             }
-        });
-    }
-
-    $(document).ready(function () {
-        initPage();
+        }
     });
+}
+$(document).ready(function() {
+    initPage();
 });
