42f00b6900b4ce37513e528ca9c1350b6d5a0642
Alex Wang <ice_b0und@hotmail.com>
2016-03-27 22:37:04 +0800

    fix a typo in script

--- mongodb/recreate.js
+++ mongodb/recreate.js
@@ -1,7 +1,7 @@
 db = connect("localhost:27017/newsdb");
 
 db.news.drop();
-db.newSources.drop();
+db.newsSources.drop();
 db.createCollection("news");
 db.createCollection("newsSources");
 db.news.createIndex({ url: 1 }, { unique: true });
